ecff338ee6a33953bb582da19b24b82872c2097f
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-04-20 13:24:31 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,6 @@
 export default defineNuxtConfig({
   css: ['bulma'],
   runtimeConfig: {
-    previewModeEncryptionSecret:
-      // !> Before releasing to production, see the README
-      process.env.NUXT_ENV_PREVIEW_MODE_ENCRYPTION_SECRET ||
-      'Use NUXT_ENV_PREVIEW_MODE_ENCRYPTION_SECRET environment variable to set a random string and encrypt the preview mode cookie',
-
-    previewModePassword:
-      // !> Before releasing to production, see the README:
-      // !> be sure to set a proper password to limit the access to preview mode
-      process.env.NUXT_ENV_PREVIEW_MODE_PASSWORD || '42',
-
-    draftEnabledToken:
-      // !> Before releasing to production, see the README for information concerning security
-      // !> and be sure to use tokens with the minimum amount of permission
-      process.env.NUXT_ENV_DATOCMS_API_DRAFT_ENABLED_TOKEN ||
-      process.env.NUXT_ENV_DATOCMS_API_TOKEN,
-
     public: {
       datocms: {
         bundleSafeToken:
